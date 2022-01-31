2b275d5796033e9bf0835d96002a0ea44118fba8
Mayel de Borniol <git@mayel.space>
2022-01-31 17:29:54 +1300

    WIP

--- lib/integration.ex
+++ lib/integration.ex
@@ -2,7 +2,7 @@
 Bonfire.Common.Config.require_extension_config!(:bonfire_quantify)
 
 defmodule Bonfire.Quantify do
-  def is_admin(user) do
+  def is_admin?(user) do
     if Map.get(user, :instance_admin) do
       Map.get(user.instance_admin, :is_instance_admin)
     else
--- lib/measure/graphql.ex
+++ lib/measure/graphql.ex
@@ -206,7 +206,7 @@ defmodule Bonfire.Quantify.Measures.GraphQL do
       with {:ok, user} <- GraphQL.current_user_or_not_logged_in(info),
            {:ok, measure} <- measure(%{id: id}, info) do
         cond do
-          Bonfire.Quantify.is_admin(user) ->
+          Bonfire.Quantify.is_admin?(user) ->
             {:ok, m} = Measures.update(measure, changes)
             {:ok, %{measure: m}}
 
