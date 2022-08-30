13af333d81764e31003f15501304fc2924dcd415
Mayel de Borniol <git@mayel.space>
2022-08-30 17:53:35 +1200

    Fix migration

--- priv/repo/migrations/20201205094943_import_quantify.exs
+++ priv/repo/migrations/20201205094943_import_quantify.exs
@@ -2,9 +2,7 @@ defmodule Bonfire.Repo.Migrations.ImportQuantify do
   use Ecto.Migration
 
   def change do
-    if Code.ensure_loaded?(Bonfire.Quantify.Migrations) do
-       Bonfire.Quantify.Migrations.change
+    Bonfire.Quantify.Migrations.change
        Bonfire.Quantify.Migrations.change_measure
-    end
   end
 end
