b9a4f9e83a64da928b2eb865649c576ed1acf74b
Mayel de Borniol <git@mayel.space>
2022-09-29 10:05:49 +1300

    current_user_required

--- lib/integration.ex
+++ lib/integration.ex
@@ -3,7 +3,7 @@ Bonfire.Common.Config.require_extension_config!(:bonfire_quantify)
 
 defmodule Bonfire.Quantify do
   def is_admin?(user) do
-    if Map.get(user, :instance_admin) do
+    if is_map(user) and Map.get(user, :instance_admin) do
       Map.get(user.instance_admin, :is_instance_admin)
     else
       # FIXME
