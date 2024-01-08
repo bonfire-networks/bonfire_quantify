341a1c1715d9cf017de086e594f060c20c278de3
Mayel de Borniol <git@mayel.space>
2024-01-08 17:06:30 +0000

    fix

--- lib/measure/graphql.ex
+++ lib/measure/graphql.ex
@@ -2,6 +2,7 @@
 if Code.ensure_loaded?(Bonfire.API.GraphQL) do
   defmodule Bonfire.Quantify.Measures.GraphQL do
     use Absinthe.Schema.Notation
+    import Bonfire.Common.Utils
 
     alias Bonfire.API.GraphQL
 
--- lib/unit/graphql.ex
+++ lib/unit/graphql.ex
@@ -3,6 +3,7 @@ if Code.ensure_loaded?(Bonfire.API.GraphQL) do
   defmodule Bonfire.Quantify.Units.GraphQL do
     use Absinthe.Schema.Notation
     import Untangle
+    import Bonfire.Common.Utils
 
     alias Bonfire.API.GraphQL
 
