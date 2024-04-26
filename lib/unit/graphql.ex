b0b11e16ce920440add90028cfe9fd55bbf84160
Mayel de Borniol <git@mayel.space>
2024-04-26 10:11:08 +0100

    api

--- lib/unit/graphql.ex
+++ lib/unit/graphql.ex
@@ -260,4 +260,6 @@ if Application.compile_env(:bonfire_api_graphql, :modularity) != :disabled and
       Bonfire.Common.Config.get_ext!(:bonfire_quantify, [Units, :valid_contexts])
     end
   end
+else
+  IO.warn("Skip GraphQL API")
 end
